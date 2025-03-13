1e3a0e1dc28b43b3729a9dfe34bfd4297fef4a83
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-13 09:57:37 -0300

    refactor: remove AppModule and main entry point files

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { DatabaseModule } from '@app/common';
-import { Module } from '@nestjs/common';
-
-@Module({
-  imports: [DatabaseModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
