f0dbb41c667e63c47d64c3cc74f20c1dc6ed8213
Geraldo Luiz <geraldobl58@gmail.com>
2025-03-13 15:40:36 -0300

    feat(campaigns): use ConfigService to retrieve application port

--- apps/campaigns/src/main.ts
+++ apps/campaigns/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
+
 import { CampaignsModule } from './campaigns.module';
 
 async function bootstrap() {
@@ -9,6 +11,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
 
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
